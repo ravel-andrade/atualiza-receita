79bfe8e9897f29367a3a63ff891e12c3c0349792
Ravel Andrade <ravel@ubots.com.br>
2022-05-20 00:10:45 -0300

    mais alguns detalhes da organização de pastas

--- src/test/java/com/db/atualizareceita/services/CsvServiceTest.java
+++ src/test/java/com/db/atualizareceita/services/CsvServiceTest.java
@@ -1,7 +1,5 @@
-package com.db.atualizareceita.services.services.processors;
-
+package com.db.atualizareceita.services;
 import com.db.atualizareceita.model.CsvData;
-import com.db.atualizareceita.services.processor.CsvProcessor;
 import com.db.atualizareceita.services.CsvService;
 import com.db.atualizareceita.services.ReceitaService;
 import org.junit.Test;
@@ -13,40 +11,46 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
-public class CsvProcessorTest {
+public class CsvServiceTest {
     CsvService csvService = new CsvService(new ReceitaService());
-    CsvProcessor csvProcessor = new CsvProcessor(csvService);
     String VALID_CSV = "src/test/java/resources/validCsv.csv";
     String INVALID_CSV = "src/test/java/resources/invalidCsv.csv";
-
     @Test
-    public void shouldAcceptCsvFile(){
-        Boolean result = csvProcessor.accept(VALID_CSV);
+    public void shouldReturnTrueWhenValidateCsv(){
+        boolean result = csvService.csvFileIsValid(VALID_CSV);
         assertEquals(true, result);
     }
 
     @Test
-    public void shouldNotAcceptCsvFile(){
-        Boolean result = csvProcessor.accept(INVALID_CSV);
+    public void validateCsvShouldReturnFalseWhenHeadersAreInvalid(){
+        boolean result = csvService.csvFileIsValid(INVALID_CSV);
         assertEquals(false, result);
     }
 
     @Test
-    public void shouldProcessCsvFile(){
+    public void validateCsvShouldReturnFalseWhenFileDoesntExist(){
+        boolean result = csvService.csvFileIsValid("fake.csv");
+        assertEquals(false, result);
+    }
+
+    @Test
+    public void shouldExtractDataFromCsv(){
         List<CsvData> expectedResult = buildACsvList();
-        List<CsvData> result = csvProcessor.process(VALID_CSV);
+        List<CsvData> result = csvService.extractDataFromCsv(VALID_CSV);
         assertEquals(expectedResult, result);
     }
 
     @Test(expected = RuntimeException.class)
-    public void shouldNotProcessCsvFile(){
-        csvProcessor.process(INVALID_CSV);
+    public void shouldNotExtractDataFromInvalidCsv(){
+        csvService.extractDataFromCsv(INVALID_CSV);
     }
 
     private List<CsvData> buildACsvList() {
         List<CsvData> expectedResultList = new ArrayList<>();
-        expectedResultList.add(new CsvData("101", "1223", 12.0, "I", "falha na atualizacao"));
-        expectedResultList.add(new CsvData("0101", "122256", 100.0, "A", "atualizado"));
+        expectedResultList.add(new CsvData("101", "1223", 12.0, "I"));
+        expectedResultList.add(new CsvData("0101", "122256", 100.0, "A"));
         return expectedResultList;
     }
+
+
 }
--- src/test/java/com/db/atualizareceita/services/processors/CsvProcessorTest.java
+++ src/test/java/com/db/atualizareceita/services/processors/CsvProcessorTest.java
@@ -1,5 +1,7 @@
-package com.db.atualizareceita.services.services;
+package com.db.atualizareceita.services.processors;
+
 import com.db.atualizareceita.model.CsvData;
+import com.db.atualizareceita.services.processor.CsvProcessor;
 import com.db.atualizareceita.services.CsvService;
 import com.db.atualizareceita.services.ReceitaService;
 import org.junit.Test;
@@ -11,46 +13,40 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
-public class CsvServiceTest {
+public class CsvProcessorTest {
     CsvService csvService = new CsvService(new ReceitaService());
+    CsvProcessor csvProcessor = new CsvProcessor(csvService);
     String VALID_CSV = "src/test/java/resources/validCsv.csv";
     String INVALID_CSV = "src/test/java/resources/invalidCsv.csv";
+
     @Test
-    public void shouldReturnTrueWhenValidateCsv(){
-        boolean result = csvService.csvFileIsValid(VALID_CSV);
+    public void shouldAcceptCsvFile(){
+        Boolean result = csvProcessor.accept(VALID_CSV);
         assertEquals(true, result);
     }
 
     @Test
-    public void validateCsvShouldReturnFalseWhenHeadersAreInvalid(){
-        boolean result = csvService.csvFileIsValid(INVALID_CSV);
+    public void shouldNotAcceptCsvFile(){
+        Boolean result = csvProcessor.accept(INVALID_CSV);
         assertEquals(false, result);
     }
 
     @Test
-    public void validateCsvShouldReturnFalseWhenFileDoesntExist(){
-        boolean result = csvService.csvFileIsValid("fake.csv");
-        assertEquals(false, result);
-    }
-
-    @Test
-    public void shouldExtractDataFromCsv(){
+    public void shouldProcessCsvFile(){
         List<CsvData> expectedResult = buildACsvList();
-        List<CsvData> result = csvService.extractDataFromCsv(VALID_CSV);
+        List<CsvData> result = csvProcessor.process(VALID_CSV);
         assertEquals(expectedResult, result);
     }
 
     @Test(expected = RuntimeException.class)
-    public void shouldNotExtractDataFromInvalidCsv(){
-        csvService.extractDataFromCsv(INVALID_CSV);
+    public void shouldNotProcessCsvFile(){
+        csvProcessor.process(INVALID_CSV);
     }
 
     private List<CsvData> buildACsvList() {
         List<CsvData> expectedResultList = new ArrayList<>();
-        expectedResultList.add(new CsvData("101", "1223", 12.0, "I"));
-        expectedResultList.add(new CsvData("0101", "122256", 100.0, "A"));
+        expectedResultList.add(new CsvData("101", "1223", 12.0, "I", "falha na atualizacao"));
+        expectedResultList.add(new CsvData("0101", "122256", 100.0, "A", "atualizado"));
         return expectedResultList;
     }
-
-
 }
